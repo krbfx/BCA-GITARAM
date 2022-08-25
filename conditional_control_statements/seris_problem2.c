e22f3fa65634e6e76c23dc661b984437497353a5
krbfx <krbfxofficial@gmail.com>
2022-08-25 07:29:20 -0800

    24.08.22

---
+++ conditional_control_statements/seris_problem2.c
@@ -0,0 +1,19 @@
+// WAP to find the result of
+// s = 1-2+3-4+5-...n
+#include<stdio.h>
+void main()
+{
+    int n,j,s=0;
+    printf("--Program to find out the result of s = 1-2+3-4+5-...n\n");
+    printf("Enter a number\n");
+    scanf("%d",&n);
+
+    for(j=1;j<=n;j++)
+    {
+        if(j%2==1)
+            s += j;
+        else
+            s -= j;
+    }
+    printf("s = %d",s);
+}
